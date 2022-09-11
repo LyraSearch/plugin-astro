66869070f63f75734142c1faefc057b11e3bce9f
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-09-11 18:58:10 +0200

    feat: log generation time
    Signed-off-by: Andres Correa Casablanca <castarco@coderspirit.xyz>

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import {
 } from '@lyrasearch/lyra'
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { compile } from 'html-to-text'
+import { getLogger } from './logger'
 import { join as joinPath } from 'node:path'
 
 export const defaultSchema: PropertiesSchema = {
@@ -101,6 +102,9 @@ export const createPlugin = (
 	options: Record<string, LyraOptions>,
 ): AstroIntegration => {
 	let config: AstroConfig
+
+	const logger = getLogger(PKG_NAME)
+
 	return {
 		name: PKG_NAME,
 		hooks: {
@@ -108,6 +112,8 @@ export const createPlugin = (
 				config = cfg
 			},
 			'astro:build:done': ({ pages, routes }) => {
+				const start = new Date().getTime()
+
 				const assetsDir = joinPath(config.outDir.pathname, 'assets')
 				if (!existsSync(assetsDir)) {
 					mkdirSync(assetsDir)
@@ -122,6 +128,9 @@ export const createPlugin = (
 						{ encoding: 'utf8' },
 					)
 				}
+
+				const ellapsed = new Date().getTime() - start
+				logger.success(`Lyra indices generated in ${ellapsed / 1000}s`)
 			},
 		},
 	}
